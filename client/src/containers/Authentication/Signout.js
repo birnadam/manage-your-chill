a01b2a208afec4976489a91408d587ea85076600
birna <briandam26@yahoo.com>
2019-11-12 18:02:52 -0800

    made conditional rendering on nav bar for whether the user is signed in

--- client/src/containers/Authentication/Signout.js
+++ client/src/containers/Authentication/Signout.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { signout } from "../../actions";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import history from "../../history";
 
 class Signout extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.signout();
+    history.push("/");
   }
 
-  render(){
-    return <h1>Sorry to see you go. $20</h1>;
+  render() {
+    return <h1>Sorry to see you go</h1>;
   }
 }
-
 
 export default connect(null, { signout })(Signout);

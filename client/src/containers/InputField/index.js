81d09fceaadba9f47decec41251642948f88e2dd
birna <briandam26@yahoo.com>
2019-11-14 14:11:49 -0800

    worked on addchiller component, icon, dialog, etc

--- client/src/containers/InputField/index.js
+++ client/src/containers/InputField/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const InputField = field => (
-  <div>
+  <div className="input">
     <input {...field.input} {...field} />
     {field.meta.touched && field.meta.error && (
       <span className="error">{field.meta.error}</span>
